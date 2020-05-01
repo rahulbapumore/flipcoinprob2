dce4c9e6f8d4d2eb3d02aad98cc3635de07175a4
Rahul <rahulbapumore@gmail.com>
2020-05-01 21:31:45 +0530

    Checking either head won or tail won or tie

--- flipcoinsimulator.sh
+++ flipcoinsimulator.sh
@@ -14,3 +14,40 @@ do
     tail=$(($tail + 1));
   fi
 done
+
+
+if [ $head == 21  ]
+then
+  echo "Head won with count $head";
+elif [ $tail == 21  ]
+then
+  echo "Tail won with count $tail";
+elif [ $head == 21  ] && [ $tail == 21  ]
+then
+  if [ $head == $tail  ]
+  then
+    while [ $head -gt $(($tail + 2))  ] || [ $tail -gt $(($head + 2 ))  ]
+    do
+      res=$(($RANDOM%2));
+      if [ res == 1  ]
+      then
+        head=$(($head + 1));
+        else
+        tail=$(($tail + 1));
+       fi
+    done
+  fi
+
+
+
+  if [ $head == 21  ]
+  then
+    echo "Head won with count $head";
+    elif [ $tail == 21  ]
+    then
+    echo "Tail won with count $tail";
+  fi
+
+
+fi
+
