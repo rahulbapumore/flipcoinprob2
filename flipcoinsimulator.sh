5d5df29a6d53f33ce490159dce72e6b017609b7b
Rahul <rahulbapumore@gmail.com>
2020-05-01 21:27:28 +0530

    tossed head till either head count become 21 or tail

--- flipcoinsimulator.sh
+++ flipcoinsimulator.sh
@@ -2,3 +2,15 @@
 
 head=0;
 tail=0;
+
+while [ $head -lt 21  ] && [ $tail -lt 21  ]
+do
+  res=$(($RANDOM % 2));
+  
+  if [ $res == 1  ]
+  then
+    head=$(($head + 1));
+  else
+    tail=$(($tail + 1));
+  fi
+done
